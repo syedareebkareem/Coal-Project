e3ef0e69fe61e264a019ec4dfaa450b59c1ff454
syedareebkareem <syedareebkareem@gmail.com>
2026-05-09 15:40:39 +0500

    Initialize data structures for steganography
    Added data declarations for message buffer, encrypted text, and final message.

--- steganography.asm
+++ steganography.asm
@@ -1,1 +1,26 @@
 ; main code file
+.model small
+.stack 100h
+
+.data
+messageBuffer db 8,?,"$","$","$","$","$","$","$","$"
+; This messageBuffer is holding the input value a user might enter
+; We did this to hold only 8 characters
+; Dollar sign is the terminator
+; if user enters Syed and enters
+; the fifth character will act as terminator and stops the input before reaching 8th value
+
+encryptedText db 9,?,"$","$","$","$","$","$","$","$","$"
+; this holds 9 array characters as last character will hold the terminator $ which doesnt count
+
+finalMessage db 9,?,"$","$","$","$","$","$","$","$","$"
+; this will also hold 9 array characters as last character will hold the terminator $ which doesnt count
+
+messageLength db ?
+; we store message length here and db is used because we need to match register size too
+
+xorKey db 05h  ;
+;xorKey is used to encrypt the data
+;we might change it to perform dynamically if time remains 
+
+.code
